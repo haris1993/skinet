0050dc43dd22ef715d560fea40b477bc8c7ef983
Haris Agić <HarisAgic@hotmail.com>
2021-06-09 01:38:46 +0200

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,14 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
            
         }
 
@@ -56,6 +64,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
